4761b5071518adc88b2ada5a1a2ffaa3842bf89e
Sh3nr0n <mehdi.lefeuvre@gmail.com>
2018-04-26 21:27:58 +0200

    code optimized

--- app/src/components/Line.js
+++ app/src/components/Line.js
@@ -14,34 +14,25 @@ import { Row, Col, Button } from 'react-materialize';
 
 //Import the image background for each column of a garden line
 import ground from './ground.png';
+const colBackground = <img src={ground} alt="groundImage" />;
+
+//Create a component and set it up as a property (by calling the "props" method) of the class it will be called in. Parentheses are optional for arrow functions "()=>"" when there's only one parameter name. As we return an expression the parentheses are not mandatory after the arrow function, however it helps the understanding of the code. 
+
+
+//Buttons : Add an event listener with onClick (JS) and link it to the property (props) of its component e.g. the ParentComponent which posesses the "addChild" and "removeChild" proerties which are linked to the function to call on click
+
+
+
+//For testing purpose : Add a js expression with {} to display the number property of the corresponding child
+
 
 export class Line extends React.Component {
     // Initial state of the class/object, we set up a class/property called "numChildren" equal to 0
-    state = {
-        numChildren: 0
-    }
-
-    render() {
-        console.log(this.state);
-
-        //A const can't be declared twice in the same block nor change its value after it has been declared, however we can add an element to an array declared with "const" e.g. the []
-        //Create a children variable
-        const children = [];
-
-        //Iterate the state numChildren of the current object 
-        for (let i = 0; i < this.state.numChildren; i += 1) {
-            //React good practices : each property displayed must receive a key so that React is able to uniquely identify it, it enhances React performances (delete key for demo)
-            //Push an element to the children variable, pass a new key and number to each new element to render, the new element is created from a component declared outside of the class
-            children.push(<ChildComponent key={i} number={i} />);
+    constructor(props) {
+        super();
+        this.state = {
+            numChildren: 0
         };
-
-        //
-        return (
-            //Return the parent element declared outside of the class and pass it 2 states, Pass 2 properties to the element (addChild and removeChild) and link them to the corresponding functions, this refers to the direct parent element e.g. "ParentComponent", then insert the child component into the parent component
-            <ParentComponent addChild={this.onAddChild} removeChild={this.onRemoveChild}>
-                {children}
-            </ParentComponent>
-        );
     }
 
     onAddChild = () => {
@@ -56,56 +47,64 @@ export class Line extends React.Component {
             numChildren: this.state.numChildren - 1
         });
     }
+
+    render() {
+
+        //A const can't be declared twice in the same block nor change its value after it has been declared, however we can add an element to an array declared with "const" e.g. the []
+        //Create a children variable
+        const children = [];
+
+        //Iterate the state numChildren of the current object 
+        for (let i = 0; i < this.state.numChildren; i += 1) {
+            //React good practices : each property displayed must receive a key so that React is able to uniquely identify it, it enhances React performances (delete key for demo)
+            //Push an element to the children variable, pass a new key and number to each new element to render, the new element is created from a component declared outside of the class
+            children.push(
+                <div key={i} number={i}>
+                    <Row>
+                        <Col s={1} className='grid-example'>{colBackground}</Col>
+                        <Col s={1} className='grid-example'>{colBackground}</Col>
+                        <Col s={1} className='grid-example'>{colBackground}</Col>
+                        <Col s={1} className='grid-example'>{colBackground}</Col>
+                        <Col s={1} className='grid-example'>{colBackground}</Col>
+                        <Col s={1} className='grid-example'>{colBackground}</Col>
+                        <Col s={1} className='grid-example'>{colBackground}</Col>
+                        <Col s={1} className='grid-example'>{colBackground}</Col>
+                        <Col s={1} className='grid-example'>{colBackground}</Col>
+                        <Col s={1} className='grid-example'>{colBackground}</Col>
+                        <Col s={2} className='grid-example'>{"Ligne n°" + this.number}</Col>
+                    </Row>
+                </div>
+            );
+        };
+
+        //
+        return (
+            //Return the parent element declared outside of the class and pass it 2 states, Pass 2 properties to the element (addChild and removeChild) and link them to the corresponding functions, this refers to the direct parent element e.g. "ParentComponent", then insert the child component into the parent component
+            <div>
+                <Row>
+                    <Col s={1} className='grid-example'>{colBackground}</Col>
+                    <Col s={1} className='grid-example'>{colBackground}</Col>
+                    <Col s={1} className='grid-example'>{colBackground}</Col>
+                    <Col s={1} className='grid-example'>{colBackground}</Col>
+                    <Col s={1} className='grid-example'>{colBackground}</Col>
+                    <Col s={1} className='grid-example'>{colBackground}</Col>
+                    <Col s={1} className='grid-example'>{colBackground}</Col>
+                    <Col s={1} className='grid-example'>{colBackground}</Col>
+                    <Col s={1} className='grid-example'>{colBackground}</Col>
+                    <Col s={1} className='grid-example'>{colBackground}</Col>
+                    <Col s={2} className='grid-example'>
+
+                        <Button className="btn-floating btn-large waves-effect waves-light red" onClick={() => this.onAddChild()}><i className="material-icons">add</i>
+                        </Button>
+                        <Button className="btn-floating btn-large waves-effect waves-light yellow" onClick={() => this.onRemoveChild()}><i className="material-icons">remove</i>
+                        </Button>
+                    </Col>
+                </Row>
+                <div>
+                    {children}
+                </div>
+            </div>
+
+        );
+    }
 }
-
-const colBackground = <img src={ground} alt="groundImage" />;
-
-//Create a component and set it up as a property (by calling the "props" method) of the class it will be called in. Parentheses are optional for arrow functions "()=>"" when there's only one parameter name. As we return an expression the parentheses are not mandatory after the arrow function, however it helps the understanding of the code. 
-const ParentComponent = props => (
-    <div>
-        <Row>
-            <Col s={1} className='grid-example'>{colBackground}</Col>
-            <Col s={1} className='grid-example'>{colBackground}</Col>
-            <Col s={1} className='grid-example'>{colBackground}</Col>
-            <Col s={1} className='grid-example'>{colBackground}</Col>
-            <Col s={1} className='grid-example'>{colBackground}</Col>
-            <Col s={1} className='grid-example'>{colBackground}</Col>
-            <Col s={1} className='grid-example'>{colBackground}</Col>
-            <Col s={1} className='grid-example'>{colBackground}</Col>
-            <Col s={1} className='grid-example'>{colBackground}</Col>
-            <Col s={1} className='grid-example'>{colBackground}</Col>
-            <Col s={2} className='grid-example'>
-            
-                <Button className="btn-floating btn-large waves-effect waves-light red" onClick={props.addChild}><i className="material-icons">add</i>
-                </Button>
-                <Button className="btn-floating btn-large waves-effect waves-light yellow" onClick={props.removeChild}><i className="material-icons">remove</i>
-                </Button>
-            </Col>
-        </Row>
-        <div>
-            {props.children}
-        </div>
-    </div>
-);
-
-//Buttons : Add an event listener with onClick (JS) and link it to the property (props) of its component e.g. the ParentComponent which posesses the "addChild" and "removeChild" proerties which are linked to the function to call on click
-
-const ChildComponent = props => (
-
-    <div>
-        <Row>
-            <Col s={1} className='grid-example'>{colBackground}</Col>
-            <Col s={1} className='grid-example'>{colBackground}</Col>
-            <Col s={1} className='grid-example'>{colBackground}</Col>
-            <Col s={1} className='grid-example'>{colBackground}</Col>
-            <Col s={1} className='grid-example'>{colBackground}</Col>
-            <Col s={1} className='grid-example'>{colBackground}</Col>
-            <Col s={1} className='grid-example'>{colBackground}</Col>
-            <Col s={1} className='grid-example'>{colBackground}</Col>
-            <Col s={1} className='grid-example'>{colBackground}</Col>
-            <Col s={1} className='grid-example'>{colBackground}</Col>
-            <Col s={2} className='grid-example'>{"Ligne n°" + props.number}</Col>
-        </Row>
-</div>);
-
-//For testing purpose : Add a js expression with {} to display the number property of the corresponding child
